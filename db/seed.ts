f1ffb6b29714b8b3dc8ae4c19e17923fb4356eae
Md Momenuzzaman <momenuzzamanemon@gmail.com>
2025-11-29 02:38:40 +0600

    added prisma

---
+++ db/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import sampleData from "./sample-data";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  try {
+    // Delete all existing products
+    await prisma.product.deleteMany({});
+
+    for (const product of sampleData.products) {
+      await prisma.product.create({
+        data: {
+          name: product.name,
+          slug: product.slug,
+          category: product.category,
+          description: product.description,
+          image: product.images ?? [],
+          price: product.price,
+          brand: product.brand,
+          rating: product.rating,
+          numReviews: product.numReviews,
+          stock: product.stock,
+          isFeatured: product.isFeatured,
+          banner: product.banner ?? undefined, // optional field
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
