4a813c197063edc5be343550bcf418a1947bd6e2
Md Momenuzzaman <momenuzzamanemon@gmail.com>
2025-12-03 01:43:58 +0600

    sign out and sign up function created

---
+++ components/shared/header/user-button.tsx
@@ -0,0 +1,71 @@
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signOutUser } from "@/lib/actions/user.action";
+import { UserIcon } from "lucide-react";
+import Link from "next/link";
+
+const UserButton = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <Button asChild>
+        <Link href="/sign-in">
+          <UserIcon className="mr-2" /> Sign-in
+        </Link>
+      </Button>
+    );
+  }
+
+  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "U";
+
+  return (
+    <div className="flex gap-2 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="relative w-8 h-8 rounded-full ml-2 bg-gray-200"
+          >
+            {firstInitial}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </p>
+              <p className="text-xs text-muted-foreground leading-none">
+                {session.user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="p-0">
+            <form action={signOutUser} className="w-full">
+              <Button
+                type="submit"
+                className="w-full justify-start"
+                variant="ghost"
+                size="sm"
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default UserButton;
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import { signIn, signOut } from "@/auth";
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import prisma from "@/db/prisma";
+
 export async function signInWithCredentials(
   prevState: unknown,
   formData: FormData
@@ -23,5 +26,39 @@ export async function signInWithCredentials(
 }
 
 export async function signOutUser() {
-  signOut();
+  await signOut();
+}
+
+//sign
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+    await signIn("credentials", { email: user.email, password: plainPassword });
+
+    return {
+      success: true,
+      message: "User registered successfully",
+    };
+  } catch (err) {
+    if (isRedirectError(err)) {
+      throw err;
+    }
+    return { success: false, message: "User was not registered" };
+  }
 }
